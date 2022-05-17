913222695c08f3aaf30e09d7688d7b435b748d69
rkota(Rosaire Kota) <rosairekota@gmail.com>
2022-05-17 17:39:59 +0100

    feat: add hasMany relationship

--- app/Models/Faculity.php
+++ app/Models/Faculity.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Faculity extends Model
 {
@@ -12,4 +13,9 @@ class Faculity extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function students()
+    {
+        return $this->hasMany(Student::class);
+    }
 }
